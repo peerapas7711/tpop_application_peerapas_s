2f8f2ce6da70ad1a36bdab118844dec0c7491c7e
peerapas.s <peerapas.s@hmcinternal.biz>
2026-03-28 09:53:44 +0700

    add lib cloud_firestore

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
@@ -16,6 +17,10 @@ Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   await AppStorage.instance.init();
+
+  final db = FirebaseFirestore.instance;
+
+  await db.collection('test').add({'message': 'hello firestore'});
 
   runApp(SubscriptionApp(environment: AppEnvironment.fromDefines()));
 }
